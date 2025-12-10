59dd628bd52c52c76d40c894347c6691e45197c1
paulopassos88 <paulo.passos.pereira1@outlook.com>
2025-12-10 11:34:35 -0300

    Adicione o microsserviço mscartoes e atualize as dependências.

---
+++ mscartoes/src/main/java/br/com/passos/mscartoes/application/CartaoController.java
@@ -0,0 +1,15 @@
+package br.com.passos.mscartoes.application;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("cartoes")
+public class CartaoController {
+
+    @GetMapping
+    public String status(){
+        return "ok";
+    }
+}
